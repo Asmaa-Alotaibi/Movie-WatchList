671f0e903b7c0c2fe28b65d173e2e4c3805fd0d3
Asmaa-Alotaibi <asmaa-otaibi@yahoo.com>
2020-11-18 18:56:48 +0300

    deletebutton Done !

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -1,20 +1,25 @@
-import React from 'react'
+import React from "react";
 
-const MovieItem = ({ movie, watchedButton }) => {
+const MovieItem = ({ movie, watchedButton, deleteMovie }) => {
   return (
     <div>
       <p>{movie.name}</p>
       <button
-        type='button'
-        className='btn btn-primary'
-        onClick={() => watchedButton(movie)}>
-        {movie.watched ? 'Watch' : 'Unwatch'}
+        type="button"
+        className="btn btn-primary"
+        onClick={() => watchedButton(movie)}
+      >
+        {movie.watched ? "Watch" : "Unwatch"}
       </button>
       <a> </a>
-      <button type='button' className='btn btn-danger'>
+      <button
+        type="button"
+        className="btn btn-danger"
+        onClick={() => deleteMovie(movie)}
+      >
         Delete
       </button>
     </div>
-  )
-}
-export default MovieItem
+  );
+};
+export default MovieItem;
--- src/components/WatchList.js
+++ src/components/WatchList.js
@@ -1,51 +1,78 @@
-import React, { useState } from 'react'
-import movies from '../movies.js'
-import MovieItem from './MovieItem.js'
+import React, { useState } from "react";
+import movies from "../movies.js";
+import MovieItem from "./MovieItem.js";
 
 const WatchList = () => {
-  const [_movies, setMovie] = useState(movies)
+  const [_movies, setMovie] = useState(movies);
+
+  const deleteMovie = (movie) => {
+    const deletedList = _movies.filter((m) => m !== movie);
+    setMovie(deletedList);
+    console.log(_movies);
+  };
   const watchedButton = (movie) => {
     setMovie(
       movies.map((m) => {
-        if (m === movie) m.watched = !m.watched
-        return m
+        if (m === movie) m.watched = !m.watched;
+        return m;
       })
-    )
-  }
-  console.log(_movies)
+    );
+  };
 
-  //
   const watchList = _movies
     .filter((movie) => movie.watched)
     .map((movie, index) => (
-      <MovieItem movie={movie} key={index} watchedButton={watchedButton} />
-    ))
+      <MovieItem
+        movie={movie}
+        key={index}
+        watchedButton={watchedButton}
+        deleteMovie={deleteMovie}
+      />
+    ));
 
   const watched = _movies
     .filter((movie) => !movie.watched)
     .map((movie, index) => (
-      <MovieItem movie={movie} key={index} watchedButton={watchedButton} />
-    ))
+      <MovieItem
+        movie={movie}
+        key={index}
+        watchedButton={watchedButton}
+        deleteMovie={deleteMovie}
+      />
+    ));
+  //const [_movies, setMovie] = useState(_movies)
 
   return (
     <div>
       <div>
         <div> Here the addbar! </div>
-        <div className='container px-lg-5'>
-          <div className='row'>Watchlist </div>
-          <div className='row'>
-            <div className='col py-3 px-lg-5 border bg-light '>{watchList}</div>
+        <div className="container px-lg-5">
+          <div className="row">
+            Watchlist{" "}
+            <span class="badge badge-Light  badge-pill">
+              {" "}
+              {watchList.length}{" "}
+            </span>{" "}
+          </div>
+          <div className="row">
+            <div className="col py-3 px-lg-5 border bg-light ">{watchList}</div>
           </div>
         </div>
-        <div className='container px-lg-5'>
-          <div className='row'>Watched </div>
-          <div className='row'>
-            <div className='col py-3 px-lg-5 border bg-light '>{watched}</div>
+        <div className="container px-lg-5">
+          <div className="row">
+            Watched{" "}
+            <span class="badge badge-primery  badge-pill">
+              {" "}
+              {watched.length}{" "}
+            </span>
+          </div>
+          <div className="row">
+            <div className="col py-3 px-lg-5 border bg-light ">{watched}</div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default WatchList
+export default WatchList;
